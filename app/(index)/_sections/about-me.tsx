9f248ee1b4b33d42104c66fe73aa89bfdba22a50
Jaka Asa Baldan Ahmad <jakaasha@gmail.com>
2023-12-26 20:34:13 +0700

    chore: versioning untracked files

--- app/(index)/_sections/about-me.tsx
+++ app/(index)/_sections/about-me.tsx
@@ -1,9 +1,9 @@
 export default function AboutMe() {
   return (
     <section className="hero bg-black text-white h-full">
-      <div className="hero-content flex-col">
+      <div className="hero-content flex-col py-32">
         <h1 className="font-bold text-3xl">About Me</h1>
-        <p className="mx-auto text-center w-3/6">
+        <p className="mx-auto text-center lg:w-3/6 px-2 md:px-8">
           I&apos;m a healer whom curating intuitive and powerful
           medicines in cooperation with{' '}
           <span className="font-bold">Universal Wits</span>. With a passion for
